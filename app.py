bf8d816ca9e2c5e79190e54e1f77a77a0f6d1c06
Hemalatha S <id>
2025-04-01 14:16:10 +0530

    first program

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print(1+1)
+
+##find biggest of three numbers
+
+num1=int(input("Enter the value of num1="))
+num2=int(input("Enter the value of num2="))
+
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+
+print("Addition of two number=",sum)
+print("subtraction of two number=",sub)
+print("Multiplication of two number=",mul)
+print("Division of two number=",div)
